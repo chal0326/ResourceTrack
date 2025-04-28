c72530a16fcf6391fc680f85cab4ed5cbc075730
chal0326 <chal032687@gmail.com>
2025-04-28 15:32:48 -0400

    Added remix router updated path from src to app

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { vitePlugin as remixVite } from '@remix-run/dev';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [remixVite(), react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },

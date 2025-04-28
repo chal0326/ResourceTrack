b57101909b6ed176061f639a8f0738595052fd05
chal0326 <chal032687@gmail.com>
2025-04-28 15:51:17 -0400

    updated routes.tsx

---
+++ app/routes/index.tsx
@@ -0,0 +1,17 @@
+// app/routes/index.tsx
+import { Link } from '@remix-run/react';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '../App.tsx';
+import './index.css';
+
+
+export default function Index() {
+  return (
+    createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)  
+  );
+}
